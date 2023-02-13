779aaff71d73d008813902231ecda144c0f663b5
deiby morales <djmo-25@outlook.com>
2023-02-13 12:26:52 -0500

    Primer Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include("template/cabecera.php");?>
+
+          <div class="jumbotron text-center">
+              <h1 class="display-3">Bienvenidos al sitio de productos digitales</h1>
+              <p class="lead">Escoge el producto digital que mas te guste al mejor precio</p>
+              <hr class="my-2">
+            <img width="400" src="img/firos-nv-1wBmbnvv4TE-unsplash.jpg" class="img-thumbnail rounded mx-auto d-block" />  
+
+              <p>Más información</p>
+              <p class="lead">
+                  <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacen de productos </a>
+              </p>
+          </div>
+
+<?php include("template/pie.php");?>
+        
